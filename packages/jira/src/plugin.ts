f30ecc47ed35aaa4c149b957e2fe09b496050773
Craigory Coppola <craigorycoppola@gmail.com>
2022-03-29 14:25:20 -0400

    fix(jira): add additional error handling for failed findUser calls

--- packages/jira/src/plugin.ts
+++ packages/jira/src/plugin.ts
@@ -1,6 +1,6 @@
 import JiraApi from 'jira-client';
 
-import { CodeRubPlugin } from '@code-rub/core';
+import { CodeRubPlugin, loadConfig } from '@code-rub/core';
 
 import { createIssue } from './create-issue';
 import { findJiraUser } from './find-jira-user';
@@ -23,9 +23,17 @@ const plugin: CodeRubPlugin<JiraPluginConfig> = {
     });
   },
   processAssignments: async (assignments, config) => {
+    const userMap: Record<string, JiraApi.UserObject> = {};
+    const codeRubConfig = await loadConfig();
+    for (const uid of codeRubConfig.uids) {
+      userMap[uid] = await findJiraUser(jira, uid);
+    }
     for (const a of assignments) {
-      const user = await findJiraUser(jira, a.uid);
-      await createIssue(jira, config, user, a.filePath);
+      try {
+        await createIssue(jira, config, userMap[a.uid], a.filePath);
+      } catch {
+        console.warn(`@code-rub/jira: Failed to create a ticket for assignment (${a.uid} - ${a.filePath}`)
+      }
     }
   },
   initialConfiguration: {
@@ -35,15 +43,15 @@ const plugin: CodeRubPlugin<JiraPluginConfig> = {
         host: '{my-jira-url}',
         descriptionTemplate: [
           `Code rubbing is just like glass rubbing, it makes cleaner, shinier and better to look at. \n`,
-          `In order for that to happen, every engineer will need to pick up a random file from the solution they are working on, and make sure it follows the best practices and latest technologies, along with making sure it maintains it’s consistency across the entire solution. \n`,
+          `In order for that to happen, every engineer will need to pick up a random file from the solution they are working on, and make sure it follows the best practices and latest technologies, along with making sure it maintains it's consistency across the entire solution. \n`,
           `There are conditions for code rubbing, which are:`,
-          `- It shouldn’t take more than 20 minutes from an engineer’s day.`,
+          `- It shouldn't take more than 20 minutes from an engineer's day.`,
           `- It should target only one single file per day.`,
-          `- It shouldn’t include implementing new features or fixing bugs.`,
+          `- It shouldn't include implementing new features or fixing bugs.`,
           `- It should require more code reading than code writing.`,
-          `- It shouldn’t require a pairing session.`,
+          `- It shouldn't require a pairing session.`,
           `- It should be done continuously every single day.`,
-          `- It shouldn’t happen to the same file by the same engineer twice unless every other file has been already rubbed`,
+          `- It shouldn't happen to the same file by the same engineer twice unless every other file has been already rubbed`,
           ``,
           `Your file has already been picked, it is {fileName}`,
         ].join('\n'),
