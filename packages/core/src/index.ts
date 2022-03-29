800d796bf962913df28b56bac17793e0824c1e45
Craigory Coppola <craigorycoppola@gmail.com>
2022-03-29 14:24:50 -0400

    feat(core): export utility for reading config

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from './models';
 export * from './lib/utils';
+export { loadConfig } from './lib/load-config';
 export { worker } from './lib/main';
--- packages/core/src/lib/load-config.ts
+++ packages/core/src/lib/load-config.ts
@@ -2,11 +2,21 @@ import { join } from 'path';
 
 import { ProvidedConfig, ResolvedConfig } from '../models/config.interface';
 import { CodeRubPlugin } from '../models/plugin.interface';
-import { configFileName, extendedRequire, repoRootPath, resolvePlugin } from './utils';
+import {
+  configFileName,
+  extendedRequire,
+  repoRootPath,
+  resolvePlugin,
+} from './utils';
 
+const configMap: Record<string, ResolvedConfig> = {};
 export async function loadConfig(
   path = configFileName()
 ): Promise<ResolvedConfig> {
+  return (configMap[path] ??= await _loadConfig(path));
+}
+
+async function _loadConfig(path: string): Promise<ResolvedConfig> {
   const config: ProvidedConfig = extendedRequire(join(repoRootPath(), path));
   config.storagePath ??= '.code-rub';
   config.plugins ??= [];
